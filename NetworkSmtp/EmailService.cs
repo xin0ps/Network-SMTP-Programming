971d51ab14c5268b39707f79894f290ee825b347
Rasul Aslanov <rasulsha@code.edu.az>
2024-03-31 14:17:36 +0400

    FIrst

---
+++ NetworkSmtp/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+public static class SendEmail
+{
+
+    static public void sendEmail(string name ,string tomail, string _message)
+    {
+        string fromMail = "steptest226@gmail.com";
+        string fromPassword = "hibihdeorcokmndd";
+
+        MailMessage message = new MailMessage();
+        message.From = new MailAddress(fromMail);
+
+        message.Subject = "Test";
+        message.To.Add(new MailAddress(tomail));
+
+        message.Body = $"<html><body><p style='font-size: 50px; color: black;'>Hello,<br/><br/>MessageFrom: <span style='font-weight: bold;'> {name}:\n{_message}</span></p></body></html>";
+
+        message.IsBodyHtml = true;
+
+        var smtpClient = new SmtpClient("smtp.gmail.com")
+        {
+            Port = 587,
+            Credentials = new NetworkCredential(fromMail, fromPassword),
+            EnableSsl = true
+        };
+
+        smtpClient.Send(message);
+
+    }
+
+
+
+
+}
---
+++ NetworkSmtp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using System.Web;
+
+using HttpListener listener = new HttpListener();
+
+listener.Prefixes.Add("http://127.0.0.1:27001/");
+
+
+listener.Start();
+
+while (true)
+{
+
+    var context = listener.GetContext();
+
+    _ = Task.Run(() =>
+    {
+        HttpListenerRequest? request = context.Request;
+
+        HttpListenerResponse? response = context.Response;
+        response.ContentType = "text/html";
+        response.Headers.Add("Content-Type", "text/html");
+        response.Headers.Add("Server", "Rasul");
+        response.Headers.Add("Date", DateTime.Now.ToString());
+        using var writer = new StreamWriter(response.OutputStream);
+
+
+        var index = File.ReadAllText("index.html");
+        writer.Write(index);
+
+
+
+
+        var queryString = request.QueryString;
+        if (queryString.Count != 0)
+        {
+            string fullname = queryString["fullname"];
+            string email = queryString["email"];
+            string message = queryString["message"];
+
+            Console.WriteLine($"Name:{fullname}\n Email:{email} \nBody:{message}");
+            try
+            {
+                SendEmail.sendEmail(fullname, email, message);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+    });
+
+}
